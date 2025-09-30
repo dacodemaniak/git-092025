4b130be7ea2e90ea39bfeb2e0224bde48dae6ed9
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-09-30 10:30:23 +0200

    Switch theme

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,12 @@
 })()
 
 const switchMode = () => {
-    console.log('Have to switch mode')
+    const body = document.querySelector('body')
+    if (body.classList.contains('dark')) {
+        body.classList.remove('dark')
+        body.classList.add('light')
+    } else {
+        body.classList.remove('light')
+        body.classList.add('dark')
+    }
 }
